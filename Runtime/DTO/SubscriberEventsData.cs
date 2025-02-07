4ce040d9a63b6b9ac3484c3e339e89aaafb363de
MarinaGorbacheva <53435883+marriuss@users.noreply.github.com>
2025-02-07 12:18:13 +0400

    fix for ios

--- Runtime/DTO/SubscriberEventsData.cs
+++ Runtime/DTO/SubscriberEventsData.cs
@@ -6,11 +6,11 @@ namespace SmsAuthAPI.DTO
     [Serializable, Preserve]
     public class SubscriberEventsData
     {
-        public string san { get; set; }
-        public string phone { get; set; }
-        public string date { get; set; }
-        public string app_id { get; set; }
-        public string version { get; set; }
-        public string platform { get; set; }
+        public string san;
+        public string phone;
+        public string date;
+        public string app_id;
+        public string version;
+        public string platform;
     }
 }
